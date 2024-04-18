15896908f436cf0c74b8db885642b8adc5cafdf3
abdullahmug2332 <abdullahmug2332@gmail.com>
2024-04-18 18:11:07 +0500

    first commit

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -27,7 +27,7 @@ export default function Home() {
                 <div className="container py-5">
                     <div className="row text-center">
                         <div className="icon col-md-4 d-flex flex-column justify-content-center align-items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plane" width="110" height="110" viewBox="0 0 24 24" stroke-width="1" stroke="#fc2d2d" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plane" width="100" height="100" viewBox="0 0 24 24" stroke-width="1" stroke="#fc2d2d" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M16 10h4a2 2 0 0 1 0 4h-4l-4 7h-3l2 -7h-4l-2 2h-3l2 -4l-2 -4h3l2 2h4l-2 -7h3z" />
                             </svg>
@@ -35,7 +35,7 @@ export default function Home() {
                             <p className='iconsm'>A wonderful serenity has taken to the possession of my entire soul.</p>
                         </div>
                         <div className="icon col-md-4 d-flex flex-column justify-content-center align-items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-skyscraper" width="100" height="100" viewBox="0 0 24 24" stroke-width="1" stroke="#fc2d2d" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-skyscraper" width="90" height="90" viewBox="0 0 24 24" stroke-width="1" stroke="#fc2d2d" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M3 21l18 0" />
                                 <path d="M5 21v-14l8 -4v18" />
@@ -49,7 +49,7 @@ export default function Home() {
                             <p className='iconsm'>A wonderful serenity has taken to the possession of my entire soul.</p>
                         </div>
                         <div className="icon col-md-4 d-flex flex-column justify-content-center align-items-center ">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-check" width="100" height="100" viewBox="0 0 24 24" stroke-width="1" stroke="#fc2d2d" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-check" width="90" height="90" viewBox="0 0 24 24" stroke-width="1" stroke="#fc2d2d" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M11 18l-2 -1l-6 3v-13l6 -3l6 3l6 -3v9" />
                                 <path d="M9 4v13" />
@@ -66,18 +66,18 @@ export default function Home() {
                 <div className="container">
                     <div className="col-12"><h1 className='rt mb-4'>Recent Tours</h1></div>
                     <div className="row text-center">
-                        <div className="col-md-4 d-flex flex-column justify-content-center">
+                        <div className="col-md-4 d-flex flex-column justify-content-center p-4">
                             <img className='rtimg shadow-lg' src={fransisco} alt="" />
                             <p className="rtdate mt-3 mb-0">Aug 02, 2017</p>
                             <p className="rtxt">Fransisco</p>
                         </div>
-                        <div className="col-md-4">
-                            <img className='rtimg shadow-lg' src={maldives} alt="" />
+                        <div className="col-md-4 p-4">
+                            <img className='rtimg shadow-lg ' src={maldives} alt="" />
                             <p className="rtdate mt-3 mb-0">Oct 12, 2013</p>
                             <p className="rtxt">Maldives</p>
                         </div>
-                        <div className="col-md-4">
-                            <img className='rtimg shadow-lg' src={switzerland} alt="" />
+                        <div className="col-md-4 p-4">
+                            <img className='rtimg shadow-lg ' src={switzerland} alt="" />
                             <p className="rtdate mt-3 mb-0">Sep 04, 2019</p>
                             <p className="rtxt ">Switzerland</p>
                         </div>
